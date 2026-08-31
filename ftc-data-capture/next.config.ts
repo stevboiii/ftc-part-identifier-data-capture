7e42cbf8d87c86c6ec0b7c4a45a45798e27d2c49
callisto <88640227+stevboiii@users.noreply.github.com>
2026-08-31 00:36:47 -0500

    removedgithub pages implementation

--- ftc-data-capture/next.config.ts
+++ ftc-data-capture/next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Tells Next.js to compile into static HTML/CSS/JS
-  images: {
-    unoptimized: true, // Required for static exports
-  },
-  // Replace 'YOUR_REPOSITORY_NAME' with your actual GitHub repository name
-  basePath: '/ftc-part-identifier-data-capture', 
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
