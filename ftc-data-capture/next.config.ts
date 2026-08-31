46f73698f24d2c50b53597e2795d59673e564c74
callisto <88640227+stevboiii@users.noreply.github.com>
2026-08-31 00:19:26 -0500

    Update next.config.ts

--- ftc-data-capture/next.config.ts
+++ ftc-data-capture/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Tells Next.js to compile into static HTML/CSS/JS
+  images: {
+    unoptimized: true, // Required for static exports
+  },
+  // Replace 'YOUR_REPOSITORY_NAME' with your actual GitHub repository name
+  basePath: '/ftc-part-identifier-data-capture', 
 };
 
 export default nextConfig;
